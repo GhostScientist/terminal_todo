1e8253b4201352b4faeab858f5e01a1f74e4d904
Dakota Kim <S0281634@SalemState.edu>
2017-02-27 10:58:14 -0500

    Added sleeping

--- todo.py
+++ todo.py
@@ -12,6 +12,7 @@ import time
 
 def run():
     print("(Hi there, if at anytime you'd like to quit this program, enter 'EXIT' into any input field)")
+    time.sleep(1.0) # This program will use a lot of sleeping so the user isn't bombarded with information.
     response = raw_input("Hello! Is this your first time creating a to-do list in the terminal?\t")
     # If this is first time, will run the createDB method to create a database to store tasks.
     if response.lower() == "yes":
@@ -26,7 +27,9 @@ def run():
 def createDB():
     pwd = os.getcwd() ## Returns the current directory. I'll add some customization to this section later.
     print("Awesome! Let's get started")
+    time.sleep(0.5)
     print("Your current directory is " + pwd)
+    time.sleep(0.5)
     print("Keep track of this path! Your database will be created here!")
     sql_file = pwd + "/my_todo.db"
     connection = sqlite3.connect(sql_file)
@@ -49,6 +52,7 @@ def choice_maker():
                        3 = Remove a task
                        4 = List all tasks
                        5 = Sort tasks!""")
+    time.sleep(1.0)
     choice = raw_input("Enter the number that corresponds to your choice!    ")
     if (choice.isdigit()):
         if choice == "1":
@@ -75,22 +79,26 @@ def choice_maker():
         print("You didn't enter a valid number. :( No problem! We can start all over again! :D")
         choice_maker()
 
-def creator():
+def creator(): # Method used to create new tasks for the list.
     print("""In order to create a new task, we will need a name for the task and an integer
             ranging from 1 to 10 that rates its importance to you. I'll be checking to ensure
             your input is formatted correctly, so don't worry! :)""")
-    task = raw_input("What would you like to add to your list?")
-    rating = raw_input("Rate this tasks importance from 1 to 10!")
+    time.sleep(1.0)
+    task = raw_input("What would you like to add to your list?  ")
+    time.sleep(0.5)
+    rating = raw_input("Rate this tasks importance from 1 to 10!    ")
     sql_command = "INSERT INTO tasks VALUES "
     values = "('" + task + "', " + rating + ")"
     final_command = sql_command + values
+    time.sleep(0.5)
     double_check = raw_input("Just to be safe, is this what you'd like to add?: Task is '" + task + "' and rating is " + rating)
     if double_check.lower() == "yes":
         c.execute(final_command) # Adds the desired task and rating into the table.
     else:
         print("That's okay, let's start over! :)")
         creator()
-#def compete():
+
+#def compete(): This method will complete a task and add that task to list of completed tasks
 
 #def remove():
 
